428c6578f1f92df8a08cf598d8541b09e4b4a8ef
lucwise-13257 <luca_lise@icloud.com>
2024-04-19 22:08:10 -0700

    Edit Build Batch

--- cleanBuild.bat
+++ cleanBuild.bat
@@ -1,4 +1,10 @@
 rmdir /S build
 mkdir build
 cmake -B build
-cmake --build build
+IF "%1" == "-r" (
+    echo "Building RELEASE"
+    cmake --build build --config Release
+) ELSE (
+    echo "Building DEBUG"
+    cmake --build build
+)
