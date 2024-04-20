7de7a0e75001fdc9b8d2755d5d443f045661e101
lucwise-13257 <luca_lise@icloud.com>
2024-04-19 22:34:59 -0700

    Add panel

--- main.cpp
+++ main.cpp
@@ -17,7 +17,12 @@ private:
 wxIMPLEMENT_APP(App);
 
 MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Downloader") {
-  
+  wxPanel* panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+  panel->SetBackgroundColour(wxColor(32,54,98));
+  wxBoxSizer* vertSizer = new wxBoxSizer(wxVERTICAL);
+
+
+  this->Center(wxBOTH);
 };
 
 bool App::OnInit() {
