97fd0a6f0f961581a533b6f1602a689fa5ab4af2
lucwise-13257 <87954986+lucwise-13257@users.noreply.github.com>
2024-04-22 01:36:10 -0700

    Sockets! (#3)
    * unfinished
    
    * Progress
    
    * Sockets!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
 #include <wx/wx.h>
+#include <sockpp/tcp_connector.h>
+
+using std::cout;
+using std::endl;
 
 class App : public wxApp {
 public:
@@ -12,16 +19,38 @@ public:
 
 private:
   void OnExit(wxCommandEvent& event);
+  void SendPing(wxCommandEvent& event);
+};
+
+enum {
+  FILE_MENU = 1,
+  MSG_BOX = 2,
+  SEND_PING = 3
 };
 
 wxIMPLEMENT_APP(App);
 
-MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Downloader") {
-  wxPanel* panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-  panel->SetBackgroundColour(wxColor(32,54,98));
+MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "SimpleChat") {
+  // wxPanel* panel = new wxPanel(this);
+  this->SetBackgroundColour(wxColor(32,54,98));
+  // panel->SetBackgroundColour(wxColor(32,54,98));
+  // Menu Stuff
+  wxMenuBar* menuBar = new wxMenuBar;
+  wxMenu* fileMenu = new wxMenu;
+  fileMenu->Append(FILE_MENU, "Quit");
+  fileMenu->Append(SEND_PING, "Send Ping");
+  Bind(wxEVT_MENU, &MainFrame::OnExit, this, FILE_MENU);
+  Bind(wxEVT_MENU, &MainFrame::SendPing, this, SEND_PING);
+  menuBar->Append(fileMenu, "File");
+  SetMenuBar(menuBar);
+
+  wxTextCtrl* msgBox = new wxTextCtrl(this, MSG_BOX);
+  msgBox->SetValue("PlaceHolder");
   wxBoxSizer* vertSizer = new wxBoxSizer(wxVERTICAL);
-
-
+  // vertSizer->Add(panel, 1, wxEXPAND | wxALL);
+  vertSizer->Add(msgBox, 0, wxALIGN_CENTER | wxLEFT | wxRIGHT | wxBOTTOM);
+  this->SetSizer(vertSizer);
+  this->SetSize(wxSize(800, 600));
   this->Center(wxBOTH);
 
 };
@@ -29,5 +58,21 @@ MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Downloader") {
 bool App::OnInit() {
   MainFrame* mainFrame = new MainFrame();
   mainFrame->Show();
+  sockpp::initialize();
   return true;
+};
+
+
+void MainFrame::OnExit(wxCommandEvent& event) {
+  Close();
+};
+
+void MainFrame::SendPing(wxCommandEvent& event) {
+  in_port_t port = 2547;
+  sockpp::tcp_connector conn({"localhost", port}, std::chrono::seconds(5));
+  if (!conn) {
+    wxMessageDialog* dialog = new wxMessageDialog(this, "Error connecting to socket");
+  };
+  conn.write_n("Ping from SimpleChat", 21);
+  wxMessageDialog* dialog = new wxMessageDialog(this, "Sent Packet");
 };
