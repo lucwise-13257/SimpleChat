e19cfb15604c340bca38947515418a95da99236c
lucwise-13257 <luca_lise@icloud.com>
2024-04-24 00:23:40 -0700

    Option for ninja and msbuild

--- cleanBuild.bat
+++ cleanBuild.bat
@@ -1,6 +1,13 @@
 rmdir /S build
 mkdir build
-cmake -B build
+IF "%2" == "-ms" (
+  echo USING MSBUILD
+  cmake -B build
+) ELSE (
+  echo USING NINJA
+  cmake -B build -G Ninja
+)
+
 IF "%1" == "-r" (
     echo "Building RELEASE"
     cmake --build build --config Release
