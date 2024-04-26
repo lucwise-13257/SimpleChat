a7b3121e4cefd9fb16a565a6729718e003a70589
lucwise-13257 <luca_lise@icloud.com>
2024-04-25 19:38:41 -0700

    Builder

--- cleanBuild.bat
+++ cleanBuild.bat
@@ -1,17 +1,55 @@
-rmdir /S build
-mkdir build
-IF "%2" == "-ms" (
-  echo USING MSBUILD
-  cmake -B build
-) ELSE (
-  echo USING NINJA
-  cmake -B build -G Ninja
+@echo off
+set BuildOption="Debug"
+set BuildSystem="Ninja"
+
+FOR %%A IN (%*) DO (
+  IF "%%A" == "-r" (
+    echo Setting Release
+    set BuildOption="Release"
+  )
+  IF "%%A" == "-ms" (
+    echo Setting MSBuild
+    set BuildSystem="MSBuild"
+  )
 )
 
-IF "%1" == "-r" (
-    echo "Building RELEASE"
-    cmake --build build --config Release
-) ELSE (
-    echo "Building DEBUG"
-    cmake --build build
+echo Building With These Options:
+echo BuildType: %BuildOption%
+echo BuildSystem: %BuildSystem%
+choice /C yn /M "Are these options OK?"
+IF errorlevel == 2 GOTO :eof
+
+rmdir /S /Q build
+mkdir build
+
+IF %BuildSystem% == "Ninja" (
+  echo BUILDING WITH NINJA
+  cmake -B build -G Ninja
+) ELSE IF %BuildSystem% == "MSBuild" (
+  echo BUILDING WITH MSBUILD
+  cmake -B build
 )
+
+IF %BuildOption% == "Release" (
+  echo RELEASE BUILD
+  cmake --build build --config Release
+) ELSE (
+  echo DEBUG BUILD
+  cmake --build build
+)
+
+@REM IF "%2" == "-ms" (
+@REM   echo USING MSBUILD
+@REM   cmake -B build
+@REM ) ELSE (
+@REM   echo USING NINJA
+@REM   cmake -B build -G Ninja
+@REM )
+
+@REM IF "%1" == "-r" (
+@REM     echo "Building RELEASE"
+@REM     cmake --build build --config Release
+@REM ) ELSE (
+@REM     echo "Building DEBUG"
+@REM     cmake --build build
+@REM )
